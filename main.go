0a50d2b595035dd1c9a48d51e78695ef9a5261b0
Rama Wandya <ramawandia153@gmail.com>
2025-09-17 01:05:12 +0700

    fix (main): change port from 80 to 5000

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	port := ":80"
+	port := ":5000"
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	log.Println("server served on ", port)
 	log.Println(http.ListenAndServe(port, nil))
